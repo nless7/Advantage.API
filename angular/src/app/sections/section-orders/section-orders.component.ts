5e35394057f693af1220582fe250c2e749fc0ad9
Stephen Cooper <coopstp@gmail.com>
2019-06-11 17:32:05 +0100

    Added create section orders todo task.

--- angular/src/app/sections/section-orders/section-orders.component.ts
+++ angular/src/app/sections/section-orders/section-orders.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionOrdersComponent implements OnInit {
 
+  //Todo: Create section orders.
+
   constructor() { }
 
   ngOnInit() {
